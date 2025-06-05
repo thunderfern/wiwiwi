8f8ab0db894821cf40112cb599da5248d45c5b06
Stwikeyy <forestwike@gmail.com>
2025-06-05 17:34:42 -0400

    i am under the water

--- wiwiwi/Assets/Water/Water.cs
+++ wiwiwi/Assets/Water/Water.cs
@@ -6,9 +6,24 @@ using UnityEngine.UIElements;
 using UnityEditor.UIElements;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer), typeof(EdgeCollider2D))]
 [RequireComponent(typeof(WaterTrigger))]
- 
+
 public class Water : MonoBehaviour
 {
+    [Header("Springs")]
+    [SerializeField] private float _springConstant = 1.4f;
+    [SerializeField] private float _damping = 1.1f;
+    [SerializeField] private float _spread = 6.5f;
+    [SerializeField, Range(1, 10)] private int _wavePropogationIterations = 8;
+    [SerializeField, Range(0f, 20f)] private float _speedMult = 5.5f;
+
+    [Header("Force")]
+    public float ForceMultiplier = 0.2f;
+    [Range(1f, 50f)] public float MaxForce = 5f;
+
+    [Header("Collision")]
+    [SerializeField, Range(1f, 10f)] private float _playerCollisionRadiusMult = 4.15f;
+
+
     [Header("Mesh Generation")]
     [Range(2, 500)] public int NumOfXVertices = 70;
     public float Width = 10f;
@@ -25,13 +40,74 @@ public class Water : MonoBehaviour
     private int[] _topVerticesIndex;
     private EdgeCollider2D _coll;
 
-    private void Start() {
-        GenerateMesh(); 
+    private class WaterPoint
+    {
+        public float velocity, acceleration, pos, targetHeight;
     }
 
-    private void Reset() {
+    private List<WaterPoint> _waterPoints = new List<WaterPoint>();
+
+    private void Start()
+    {
+        _coll = GetComponent<EdgeCollider2D>();
+
+        GenerateMesh();
+        CreateWaterPoints();
+    }
+
+    private void Reset()
+    {
         _coll = GetComponent<EdgeCollider2D>();
         _coll.isTrigger = true;
+    }
+
+    private void FixedUpdate()
+    {
+        //update all spring positions
+        for (int i = 1; i < _waterPoints.Count - 1; i++)
+        {
+            WaterPoint point = _waterPoints[i];
+            float x = point.pos - point.targetHeight;
+            float acceleration = -_springConstant * x - _damping * point.velocity;
+            point.pos += point.velocity * _speedMult * Time.fixedDeltaTime;
+            _vertices[_topVerticesIndex[i]].y = point.pos;
+            point.velocity += acceleration * _speedMult * Time.fixedDeltaTime;
+
+        }
+        //wave propogation
+        for (int j = 0; j < _wavePropogationIterations; j++)
+        {
+            for (int i = 1; i < _waterPoints.Count - 1; i++)
+            {
+                float leftDelta = _spread * (_waterPoints[i].pos - _waterPoints[i - 1].pos) * _speedMult * Time.fixedDeltaTime;
+                _waterPoints[i - 1].velocity += leftDelta;
+                float rightDelta = _spread * (_waterPoints[i].pos - _waterPoints[i + 1].pos) * _speedMult * Time.fixedDeltaTime;
+                _waterPoints[i + 1].velocity += rightDelta;
+            }
+        }
+        //update the mesh
+        _mesh.vertices = _vertices; 
+
+    }
+
+    public void Splash(Collider2D collision, float force)
+    {
+        float radius = collision.bounds.extents.x * _playerCollisionRadiusMult;
+        Vector2 center = collision.transform.position;
+        for (int i = 0; i < _waterPoints.Count; i++)
+        {
+            Vector2 vertexWorldPos = transform.TransformPoint(_vertices[_topVerticesIndex[i]]);
+            if (IsPointInsideCircle(vertexWorldPos, center, radius))
+            {
+                _waterPoints[i].velocity = force;
+            }
+        }
+    }
+
+    private bool IsPointInsideCircle(Vector2 point, Vector2 center, float radius)
+    {
+        float distanceSquared = (point - center).sqrMagnitude;
+        return distanceSquared <= radius * radius;
     }
 
     public void ResetEdgeCollider()
@@ -44,7 +120,7 @@ public class Water : MonoBehaviour
         newPoints[1] = secondPoint;
 
         _coll.offset = Vector2.zero;
-        _coll.points = newPoints; 
+        _coll.points = newPoints;
     }
 
     public void GenerateMesh()
@@ -120,6 +196,20 @@ public class Water : MonoBehaviour
             _mesh.RecalculateBounds();
 
             _meshFilter.mesh = _mesh;
+        }
+    }
+
+    private void CreateWaterPoints()
+    {
+        _waterPoints.Clear();
+        for (int i = 0; i < _topVerticesIndex.Length; i++)
+        {
+            _waterPoints.Add(new WaterPoint
+            {
+                pos = _vertices[_topVerticesIndex[i]].y,
+                targetHeight = _vertices[_topVerticesIndex[i]].y,
+
+            });
         }
     }
 
@@ -226,5 +316,6 @@ public class WaterEditor : Editor
 
 
     }
+
     
 }
--- wiwiwi/Assets/Water/WaterTrigger.cs
+++ wiwiwi/Assets/Water/WaterTrigger.cs
@@ -3,14 +3,51 @@ using UnityEngine;
 public class WaterTrigger : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private LayerMask _waterMask;
+    [SerializeField] private GameObject _splashParticles;
+
+    private EdgeCollider2D _edgeColl;
+    private Water _water;
+    private void Awake()
     {
+        _edgeColl = GetComponent<EdgeCollider2D>();
+        _water = GetComponent<Water>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((_waterMask.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            Rigidbody2D rb = collision.GetComponentInParent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                Vector2 localPos = gameObject.transform.localPosition;
+                Vector2 hitObjectPos = collision.transform.position;
+                Bounds hitObjectBounds = collision.bounds;
+                Vector3 spawnPos = Vector3.zero;
+                if (collision.transform.position.y >= _edgeColl.points[1].y + _edgeColl.offset.y + localPos.y)
+                {
+                    spawnPos = hitObjectPos - new Vector2(0f, hitObjectBounds.extents.y);
+                }
+                else
+                {
+                    spawnPos = hitObjectPos + new Vector2(0f, hitObjectBounds.extents.y);
+
+                }
+                Instantiate(_splashParticles, spawnPos, Quaternion.identity);
+
+                int multiplier = 1;
+                if (rb.linearVelocity.y < 0)
+                {
+                    multiplier = -1;
+                }else{ multiplier = 1; }
+                float vel = rb.linearVelocity.y * _water.ForceMultiplier;
+                vel = Mathf.Clamp(Mathf.Abs(vel), 0f, _water.MaxForce);
+                vel *= multiplier;
+                _water.Splash(collision, vel);
+            }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
